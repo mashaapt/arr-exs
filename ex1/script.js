21ed5ad38c51d7dcf6df3810d8e218b7c8457dcd
masha <baby@Mashaa-McBook-Air.local>
2022-11-18 13:06:11 +0200

    new

---
+++ ex1/script.js
@@ -0,0 +1,64 @@
+//1
+
+// let favFood = ['lemons', 'noodles', 'peaches', 
+// 'veggies', 'seafood'];
+
+// favFood.splice(0, 2, 'garlic');
+
+// console.log(favFood)
+
+
+
+
+//2
+
+// let myString = 'Ryu+Ken+Chun-Li+Cammy+Guile+Sakura+Sagat+Juri';
+
+// let myArray = myString.split('+');
+// console.log(myArray);
+// // console.log(myString.split('+').join(' ')
+// // );
+
+// let arrayLength = myArray.length;
+// console.log(arrayLength);
+
+// let lastItem = myArray.pop();
+// console.log(lastItem);
+
+
+
+
+
+//3
+
+// let myArray = [ "Ryu", "Ken", "Chun-Li", "Cammy", "Guile", "Sakura", "Sagat", "Juri" ];
+
+// // myArray.pop();
+// // myArray.push(...['Monica', 'Mary']);
+
+// myArray.splice(myArray.length - 1, 1, 'Monica', 'Mary');
+
+// console.log(myArray);
+// section.innerHTML = ' ';
+
+// let para1 = document.createElement('p');
+// para1.textContent = myString;
+
+// section.appendChild(para1);
+
+
+
+
+
+//4
+
+// const birds = [ "Parrots", "Falcons", "Eagles", "Emus", "Caracaras", "Egrets" ];
+// // let eagle = birds.findIndex(bird => bird === 'Eagles');
+// let eagle = birds.indexOf('Eagles');
+// let remove = birds.splice(eagle, 1);
+
+// // console.log(birds);
+
+// let eBirds = birds.filter(str => str.startsWith('E'));
+
+// console.log(eBirds);
